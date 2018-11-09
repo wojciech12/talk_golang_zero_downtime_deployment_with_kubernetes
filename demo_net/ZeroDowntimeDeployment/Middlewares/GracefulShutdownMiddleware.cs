04dbc9bd82e09cb08691fcd449d7eed847b87b93
Paweł Ruciński <Pawel.Rucinski@gyldendal.no>
2018-11-09 14:31:05 +0100

    Added 5 sec delay before shutting down, added logging

--- demo_net/ZeroDowntimeDeployment/Middlewares/GracefulShutdownMiddleware.cs
+++ demo_net/ZeroDowntimeDeployment/Middlewares/GracefulShutdownMiddleware.cs
@@ -1,13 +1,16 @@
-﻿using System.Runtime.Loader;
+﻿using System;
+using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace ZeroDowntimeDeployment.Middlewares
 {
     public class GracefulShutdownMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<GracefulShutdownMiddleware> _logger;
 
         private static int _concurrentRequests;
         private readonly object _concurrentRequestsLock = new object();
@@ -17,19 +20,23 @@ namespace ZeroDowntimeDeployment.Middlewares
 
         private readonly ManualResetEventSlim _unloadingEvent = new ManualResetEventSlim();
 
-        public GracefulShutdownMiddleware(RequestDelegate next)
+        public GracefulShutdownMiddleware(RequestDelegate next,
+            ILogger<GracefulShutdownMiddleware> logger)
         {
             _next = next;
-            
+            _logger = logger;
+
             AssemblyLoadContext.Default.Unloading += OnUnloading;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
+            var requestId = Guid.NewGuid();
             lock(_shutdownLock)
             {
                 if (_shutdown)
                 {
+                    _logger.LogInformation($"GracefulShutdownMiddleware InvokeAsync {requestId} on shutdown");
                     context.Response.StatusCode = StatusCodes.Status500InternalServerError;
                     return;
                 }
@@ -40,7 +47,9 @@ namespace ZeroDowntimeDeployment.Middlewares
                 ++_concurrentRequests;
             }
 
+            _logger.LogInformation($"GracefulShutdownMiddleware starting invoking next {requestId}");
             await _next(context);
+            _logger.LogInformation($"GracefulShutdownMiddleware finished invoking next {requestId}");
 
             lock (_concurrentRequestsLock)
             {
@@ -53,17 +62,25 @@ namespace ZeroDowntimeDeployment.Middlewares
 
         private void OnUnloading(AssemblyLoadContext obj)
         {
+            _logger.LogInformation("GracefulShutdownMiddleware OnUnloading");
             lock (_shutdownLock)
             {
+                _logger.LogInformation("Setting shutdown lock");
                 _shutdown = true;
             }
 
             lock (_concurrentRequestsLock)
             {
                 if (_concurrentRequests == 0)
+                {
+                    _logger.LogInformation("No concurrent requests in progress, shutting down. In 5 sec.");
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
                     return;
+                }
             }
             _unloadingEvent.Wait();
+            _logger.LogInformation("Last requests were processed, shutting down. In 5 sec.");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
         }
     }
 }
